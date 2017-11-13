9c6b7b488a2d9de6cbfb50351ca61d4b5bb08435
4kix <falko.ilya@gmail.com>
2017-11-13 16:10:42 +0300

    Add URL invocation

--- src/main/java/com/iba/schedule/task/Task.java
+++ src/main/java/com/iba/schedule/task/Task.java
@@ -4,6 +4,9 @@ import com.iba.schedule.model.TaskResponseModel;
 import com.iba.schedule.task.interfaces.CancellableRunnable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -26,9 +29,19 @@ public class Task implements CancellableRunnable {
 
         //TODO invoke URL with RestTemplate
 
+        String result = null;
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            result = restTemplate.getForObject("https://lpb-dev.w3bmix.ibm.com/v2/setcompletion", String.class);
+        } catch (RestClientException e) {
+            logger.error("URL invoking failed");
+            taskStatus = false;
+            model.get().setCurrentStatus("INTERRUPTED");
+        }
+
         if(taskStatus){
             model.get().setCurrentStatus("OK");
-            model.get().setBody("DONE");
+            model.get().setBody(result);
         }
         logger.info("TASK STOPPED");
 
